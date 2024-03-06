83c67b148ca7fbbc1055200de4c910f1923b0950
Part <pkhillan1@gmail.com>
2024-03-06 04:37:37 -0600

    Updated a project-card and Technical Skills

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import './App.css';
 import profileImage from './pic.png';
-import img1 from './page-1.png'
+import img1 from './project-material/page-1.png';
+import icon from './github.png';
+import cImage from './skills/C.png';
+import cppImage from './skills/c++.png';
+import cssImage from './skills/css.png';
+import htmlImage from './skills/html.png';
+import javaImage from './skills/java.png';
+import jsImage from './skills/js.png';
+import linuxImage from './skills/linux.png';
+import mysqlImage from './skills/mysql.png';
+import perlImage from './skills/perl.png';
+import pythonImage from './skills/python.png';
+import reactImage from './skills/react.png';
+import rubyImage from './skills/ruby.png';
+import materialUIImage from './skills/material-ui.png';
+
 
 function App() {
   return (
@@ -43,36 +58,107 @@ function App() {
 
         <section id="projects">
           <div className="container">
-            <div className="heading">
-              <h2>Projects</h2>
+            <h2>Projects</h2>
+
+            <div className="project-card" tabIndex="0">
+              <div className="project-details">
+                <h3>Mighty-Moves</h3>
+                <p>Tech Stack: React for front-end development, React Router for navigation, CSS for styling, and Material-UI for components and layout. Designed and crafted a dynamic fitness app website using React, featuring responsive design and seamless dynamic routing. Introduced an intuitive horizontal scrolling menu to enhance user navigation and engagement. Implemented interactive exercise cards, creating an immersive and user-friendly experience. Carried out Analysis, Requirement understanding, feasibility, System designing, Implementation, and Testing.</p>
+                <a href="https://github.com/pkhillan04/fitnessApp">
+                  <img className="icons" src={icon} alt="GitHub" />
+                </a>
+              </div>
+              <img className="image project-image" src={img1} tabIndex="0" alt="Project 1 Image" />
             </div>
 
             <div className="project-card" tabIndex="0">
               <div className="project-details">
                 <h3>Mighty-Moves</h3>
-                <p>Tech Stack: React for front-end development, React Router for navigation, CSS for styling, and Material-UI for components and layout.
-                  Designed and crafted a dynamic fitness app website using React, featuring responsive design and seamless dynamic routing. Introduced an intuitive horizontal scrolling menu to enhance user navigation and engagement.
-                  Implemented interactive exercise cards, creating an immersive and user-friendly experience.
-                  Carried out Analysis, Requirement understanding, feasibility, System designing, Implementation, and Testing.</p>
-                <img src="github.png" alt="GitHub" /> Git
+                <p>Tech Stack: React for front-end development, React Router for navigation, CSS for styling, and Material-UI for components and layout. Designed and crafted a dynamic fitness app website using React, featuring responsive design and seamless dynamic routing. Introduced an intuitive horizontal scrolling menu to enhance user navigation and engagement. Implemented interactive exercise cards, creating an immersive and user-friendly experience. Carried out Analysis, Requirement understanding, feasibility, System designing, Implementation, and Testing.</p>
+                <a href="https://github.com/pkhillan04/fitnessApp">
+                  <img className="icons" src={icon} alt="GitHub" />
+                </a>
               </div>
-              <img className="image" src={img1} tabIndex="0" alt="Project 1 Image" />
+              <img className="image project-image" src={img1} tabIndex="0" alt="Project 1 Image" />
+            </div>
+
+            <div className="project-card" tabIndex="0">
+              <div className="project-details">
+                <h3>Mighty-Moves</h3>
+                <p>Tech Stack: React for front-end development, React Router for navigation, CSS for styling, and Material-UI for components and layout. Designed and crafted a dynamic fitness app website using React, featuring responsive design and seamless dynamic routing. Introduced an intuitive horizontal scrolling menu to enhance user navigation and engagement. Implemented interactive exercise cards, creating an immersive and user-friendly experience. Carried out Analysis, Requirement understanding, feasibility, System designing, Implementation, and Testing.</p>
+                <a href="https://github.com/pkhillan04/fitnessApp">
+                  <img className="icons" src={icon} alt="GitHub" />
+                </a>
+              </div>
+              <img className="image project-image" src={img1} tabIndex="0" alt="Project 1 Image" />
+            </div>
+          </div>
+        </section>
+
+        <section id="skills">
+          <h2>Technical Skills</h2>
+          <div className='container'>
+            <div className="skill">
+              <img src={cImage} alt="C" />
+              <p>C</p>
+            </div>
+            <div className="skill">
+              <img src={cppImage} alt="C++" />
+              <p>C++</p>
+            </div>
+            <div className="skill">
+              <img src={cssImage} alt="CSS" />
+              <p>CSS</p>
+            </div>
+            <div className="skill">
+              <img src={htmlImage} alt="HTML" />
+              <p>HTML</p>
+            </div>
+            <div className="skill">
+              <img src={javaImage} alt="Java" />
+              <p>Java</p>
+            </div>
+            <div className="skill">
+              <img src={jsImage} alt="JavaScript" />
+              <p>JavaScript</p>
+            </div>
+            <div className="skill">
+              <img src={linuxImage} alt="Linux" />
+              <p>Linux</p>
+            </div>
+            <div className="skill">
+              <img src={materialUIImage} alt="Material-UI" />
+              <p>Material-UI</p>
+            </div>
+            <div className="skill">
+              <img src={mysqlImage} alt="MySQL" />
+              <p>MySQL</p>
+            </div>
+            <div className="skill">
+              <img src={perlImage} alt="Perl" />
+              <p>Perl</p>
+            </div>
+            <div className="skill">
+              <img src={pythonImage} alt="Python" />
+              <p>Python</p>
+            </div>
+            <div className="skill">
+              <img src={reactImage} alt="React" />
+              <p>React</p>
+            </div>
+            <div className="skill">
+              <img src={rubyImage} alt="Ruby" />
+              <p>Ruby</p>
             </div>
           </div>
 
-        </section >
 
-        <section id="skills">
-          <h2>Skills</h2>
-          <p>Programming Languages- C, C++, Java (OOPS, Data Structures and Algorithms), Python<br />
-            Cloud & System Operations- Linux, AWS (EC2, S3, RDS, Lambda - Familiar)<br />
-            Web Development & Design- HTML, CSS, JavaScript, React, React Router, Material UI <br />
-            Databases- MySQL</p>
+
         </section>
         <footer>
           <p>&copy; 2024 Parth Khillan</p>
         </footer>
-      </div >
+      </div>
     </div >
   );
 }
