de0adbe2942cc6b981b4b2a89de1b66b7c0c10f9
Part <pkhillan1@gmail.com>
2024-03-05 21:57:32 -0600

    created project card and animations to it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
-// Import your profile image here
-// Make sure the path is correct if you move this file
 import profileImage from './pic.png';
+import img1 from './page-1.png'
 
 function App() {
   return (
@@ -43,30 +42,38 @@ function App() {
         </section>
 
         <section id="projects">
-          <h2>Projects</h2>
-          <div className="project-container">
-            {/* Repeat this <div> for each project */}
-            <div className="project-card">
-              <h3>Project Title</h3>
-              <p>Project description...</p>
-              {/* ... other project information ... */}
+          <div className="container">
+            <div className="heading">
+              <h2>Projects</h2>
             </div>
-            {/* ... other project cards ... */}
+
+            <div className="project-card" tabIndex="0">
+              <div className="project-details">
+                <h3>Mighty-Moves</h3>
+                <p>Tech Stack: React for front-end development, React Router for navigation, CSS for styling, and Material-UI for components and layout.
+                  Designed and crafted a dynamic fitness app website using React, featuring responsive design and seamless dynamic routing. Introduced an intuitive horizontal scrolling menu to enhance user navigation and engagement.
+                  Implemented interactive exercise cards, creating an immersive and user-friendly experience.
+                  Carried out Analysis, Requirement understanding, feasibility, System designing, Implementation, and Testing.</p>
+                <img src="github.png" alt="GitHub" /> Git
+              </div>
+              <img className="image" src={img1} tabIndex="0" alt="Project 1 Image" />
+            </div>
           </div>
-        </section>
+
+        </section >
 
         <section id="skills">
           <h2>Skills</h2>
-          <p>Programming Languages- C, C++, Java (OOPS, Data Structures and Algorithms), Python<br></br>
-Cloud & System Operations- Linux, AWS (EC2, S3, RDS, Lambda - Familiar)<br></br>
-Web Development & Design- HTML, CSS, JavaScript, React, React Router, Material UI <br></br>
-Databases- MySQL</p>
+          <p>Programming Languages- C, C++, Java (OOPS, Data Structures and Algorithms), Python<br />
+            Cloud & System Operations- Linux, AWS (EC2, S3, RDS, Lambda - Familiar)<br />
+            Web Development & Design- HTML, CSS, JavaScript, React, React Router, Material UI <br />
+            Databases- MySQL</p>
         </section>
         <footer>
           <p>&copy; 2024 Parth Khillan</p>
         </footer>
-      </div>
-    </div>
+      </div >
+    </div >
   );
 }
 
